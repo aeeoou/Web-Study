98c58bfe690da26494f3c294ba8a1d4ec0cec182
chimmimmi <145756968+chimmimmi@users.noreply.github.com>
2023-12-05 13:33:02 +0900

    Initial commit

---
+++ src/main/java/com/board/domain/BoardDTO.java
@@ -0,0 +1,75 @@
+package com.board.domain;
+
+import java.time.LocalDateTime;
+
+public class BoardDTO extends CommonDTO {
+	
+	private Long idx;
+	
+	private String title;
+	private String content;
+	private String writer;
+	
+	private int viewCnt;
+	
+	private String noticeYn;
+	private String secretYn;
+	private String deleteYn;
+	
+	public Long getIdx() {
+		return idx;
+	}
+	public void setIdx(Long idx) {
+		this.idx = idx;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public int getViewCnt() {
+		return viewCnt;
+	}
+	public void setViewCnt(int viewCnt) {
+		this.viewCnt = viewCnt;
+	}
+	public String getNoticeYn() {
+		return noticeYn;
+	}
+	public void setNoticeYn(String noticeYn) {
+		this.noticeYn = noticeYn;
+	}
+	public String getSecretYn() {
+		return secretYn;
+	}
+	public void setSecretYn(String secretYn) {
+		this.secretYn = secretYn;
+	}
+	public String getDeleteYn() {
+		return deleteYn;
+	}
+	public void setDeleteYn(String deleteYn) {
+		this.deleteYn = deleteYn;
+	}
+	@Override
+	public String toString() {
+		return "BoardDTO [idx=" + idx + ", title=" + title + ", content=" + content + ", writer=" + writer
+				+ ", viewCnt=" + viewCnt + ", noticeYn=" + noticeYn + ", secretYn=" + secretYn + ", deleteYn="
+				+ deleteYn + "]";
+	}
+	
+	
+}
